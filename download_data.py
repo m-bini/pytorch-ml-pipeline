75c295281423c1731578c80cfa27c8300385759d
massimobini <massimobini.w@gmail.com>
2021-06-10 20:00:28 +0200

    create nn model class

--- download_data.py
+++ download_data.py
@@ -1,11 +1,7 @@
-import torch
-from torch import nn
-from torch.utils.data import DataLoader
 from torchvision import datasets
-from torchvision import transforms
 from torchvision.transforms import ToTensor
 
-# 1 - download dataset
+# download dataset and create dataloader
 
 def download_mnist_datasets():
     train_data = datasets.MNIST(
@@ -25,4 +21,4 @@ def download_mnist_datasets():
 if __name__ == '__main__':
     # download MNIST dataset
     train_data, _ = download_mnist_datasets()
-    print("ok")
+    print("MNIST dataset downloaded")
