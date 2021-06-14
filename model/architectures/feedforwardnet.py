68ac9dc557909499f9fc206659106ec68ac8c692
massimobini <massimobini.w@gmail.com>
2021-06-14 17:52:19 +0200

    modify classes to work with model

--- model/architectures/feedforwardnet.py
+++ model/architectures/feedforwardnet.py
@@ -1,16 +1,15 @@
 from torch import nn
 
-# create model
 
 class FeedForwardNet(nn.Module):
 
     def __init__(self, parameters):
         super().__init__()
 
-        # in_dim = 784, hidden_dim = 256, out_dim = 10
         self.in_dim = parameters['in_dim']
         self.hidden_dim = parameters['hidden_dim']
         self.out_dim = parameters['out_dim']
+
         self.flatten = nn.Flatten()
         self.dense_layers = nn.Sequential(
             nn.Linear(self.in_dim, self.hidden_dim),
--- model/losses/binarycrossentropy.py
+++ model/losses/binarycrossentropy.py
@@ -1,12 +1,15 @@
 import torch.nn.functional as F
 
 class BinaryCrossEntropy:
-
     def __init__(self, parameters):
+        
         self.parameters = parameters
 
-    def __call__(self, x, y):
+    def function(self, x, y):           
         log_prob = -1.0 * F.log_softmax(x, 1)
         loss = log_prob.gather(1, y.unsqueeze(1))
         loss = loss.mean()
         return loss
+    
+    def __call__(self, x, y):
+        return self.function(x, y)
