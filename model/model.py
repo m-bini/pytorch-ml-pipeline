b2afd3eac8ebb84cb4b40a3fa4702807401d97fd
massimobini <massimobini.w@gmail.com>
2021-06-17 14:28:58 +0200

    build function to add model items one at a time

--- model/model.py
+++ model/model.py
@@ -2,7 +2,6 @@ import importlib
 import json
 
 
-
 class Model:
     def __init__(self, model_parameters=None, model_parameters_path: str=None):
 
@@ -13,10 +12,9 @@ class Model:
         # items
         self.architecture = self.create_item_instance("architecture")
         self.loss = self.create_item_instance("loss")
-        self.optimizer = self.create_item_instance("optimizer")(self.architecture.parameters())
-        self.scheduler = self.create_item_instance("scheduler")(self.optimizer)
+        self.optimizer = self.create_optimizer()
+        self.scheduler = self.create_scheduler()
     
-
     def update_model_parameters(self):
         if self.model_parameters_path:
             with open(self.model_parameters_path, 'r') as f:
@@ -37,6 +35,18 @@ class Model:
                 item_class = self.get_item_class(item)
                 return item_class(self.model_parameters[item]["PARAMETERS"])
         return
+    
+    def create_optimizer(self):
+        item_class = self.create_item_instance('optimizer')
+        if item_class and self.architecture:
+            return item_class(self.architecture.parameters())
+        return 
+    
+    def create_scheduler(self):
+        item_class = self.create_item_instance('scheduler')
+        if item_class and self.optimizer:
+            return item_class(self.optimizer)
+        return 
 
 
 """
@@ -45,10 +55,17 @@ class Model:
 model_parameters_path = "model/model_parameters.json"
 
 M = Model(model_parameters_path=model_parameters_path)
+N = Model()
 
 print("---Model\n", M)
 print("---architecture\n", M.architecture)
 print("---loss\n", M.loss)
 print("---optimizer\n", M.optimizer)
 print("---scheduler\n", M.scheduler)
+
+print("---Model\n", N)
+print("---architecture\n", N.architecture)
+print("---loss\n", N.loss)
+print("---optimizer\n", N.optimizer)
+print("---scheduler\n", N.scheduler)
 """
